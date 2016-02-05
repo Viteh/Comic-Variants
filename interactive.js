042571c2417ccce39e7a5b08f2941cfa4b64ee24
Viteh <Raul@Rauls-MacBook-Pro.local>
2016-02-04 22:00:48 -0500

    Added Some Interactivity
    Changed the css and added some jquery to light up a cover if you have
    it.

--- interactive.js
+++ interactive.js
@@ -1,3 +1,17 @@
-$(".responsive").click(function(){
-    $(".responsive").css('opacity', '1.0');
+$(document).ready(function(){
+    $("input[type='checkbox']").change(function(){
+    	if($(this).is(":checked")){
+        	$(this).parent().siblings("img").removeClass("trans"); 
+    	}else{
+        	$(this).parent().siblings("img").addClass("trans");  
+    	}
+	});
+    $("#types").hide();
+    $("#arcs").hide();
+    $(".filter h3").mouseover(function(){
+    	$("#types").show();
+    });
+    $(".wrapper").mouseover(function(){
+    	$("#types").hide();
+    });
 });
